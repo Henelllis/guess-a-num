a23054adf8e35a8d6c53d1f000e9fdbcff52ce34
henelllis <henelllis>
2020-03-14 12:23:43 -0400

    add card effect for input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -36,7 +36,15 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems:'center'
+        alignItems:'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.26,
+        shadowRadius: 6,
+        backgroundColor: 'white',
+        elevation: 8,
+        padding: 20,
+        borderRadius: 10
     }
 });
 
